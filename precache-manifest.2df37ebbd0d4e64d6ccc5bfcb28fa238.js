88ae95bf2ea0c7922e48d92b2955b820084584c3
unknown <pengliao2010@gmail.com>
2019-02-09 19:45:41 -0500

    Updates

---
+++ precache-manifest.2df37ebbd0d4e64d6ccc5bfcb28fa238.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Peeeeeng.github.io/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "ea884fab4bd1434155d7",
+    "url": "/Peeeeeng.github.io/static/js/runtime~main.ea884fab.js"
+  },
+  {
+    "revision": "414a15e14bdd6395ccab",
+    "url": "/Peeeeeng.github.io/static/js/main.414a15e1.chunk.js"
+  },
+  {
+    "revision": "f739feb7d61c48a46bed",
+    "url": "/Peeeeeng.github.io/static/js/1.f739feb7.chunk.js"
+  },
+  {
+    "revision": "414a15e14bdd6395ccab",
+    "url": "/Peeeeeng.github.io/static/css/main.2ead9171.chunk.css"
+  },
+  {
+    "revision": "42ab3e16bc45c5192844d559c182737f",
+    "url": "/Peeeeeng.github.io/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.b8747c0b003ce42a87f135450c95f0c3.js"
+  "/Peeeeeng.github.io/precache-manifest.2df37ebbd0d4e64d6ccc5bfcb28fa238.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/Peeeeeng.github.io/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
