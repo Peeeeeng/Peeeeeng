1894796eeda43f89233f1d57bdae53cc06d65045
unknown <pengliao2010@gmail.com>
2019-02-11 00:41:16 -0500

    Updates

---
+++ precache-manifest.7e20d4f10e9ad2ec12d6db0ffc302c30.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "901091018b88a70f5ab5",
+    "url": "/static/js/main.90109101.chunk.js"
+  },
+  {
+    "revision": "558dc9641e8925601be6",
+    "url": "/static/js/1.558dc964.chunk.js"
+  },
+  {
+    "revision": "901091018b88a70f5ab5",
+    "url": "/static/css/main.8a7417fa.chunk.css"
+  },
+  {
+    "revision": "5cb2a8103c643b7ba00900b5875dc9b5",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.b8747c0b003ce42a87f135450c95f0c3.js"
+  "/precache-manifest.7e20d4f10e9ad2ec12d6db0ffc302c30.js"
 );
 
 workbox.clientsClaim();
